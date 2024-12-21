ef26c2a79609beaefa19a8247d7efce3d9a9caad
gamerhub2345 <gamersworld24by7@gmail.com>
2024-12-21 13:01:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,8 +21,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7562700347:AAF8FJcGopJkxlZbI2I3766UZLcOkJ5
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5643587914').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/venom2747") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002400159709'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/newallmovies906')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002254845416').split()]
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/allnewmovies486')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '5643587914').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://allmoviespb:allmoviespb@allmoviespb.rvt7d.mongodb.net/?retryWrites=true&w=majority&appName=allmoviespb")
